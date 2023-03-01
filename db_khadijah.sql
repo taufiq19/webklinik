c41faf9def3afb2a72b4cf27672535043543dc6e
Teha Project <htaufiq007@Gmail.com>
2023-03-01 15:21:53 +0800

    DATABASE

---
+++ db_khadijah.sql
@@ -0,0 +1,399 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 01, 2023 at 08:21 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_khadijah`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `about`
+--
+
+CREATE TABLE `about` (
+  `id` int(11) NOT NULL,
+  `about` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `about`
+--
+
+INSERT INTO `about` (`id`, `about`) VALUES
+(1, '<p>hjhjsansmssss</p>\r\n');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nama` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id_admin`, `username`, `password`, `nama`) VALUES
+(1, 'ok', '123', 'Taufiq Hidayat');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `berita`
+--
+
+CREATE TABLE `berita` (
+  `id_berita` int(11) NOT NULL,
+  `id_admin` int(11) NOT NULL,
+  `judul_artikel` varchar(255) NOT NULL,
+  `jenis_spesialis` varchar(255) NOT NULL,
+  `isiberita` text NOT NULL,
+  `tanggal` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `tag` varchar(50) NOT NULL,
+  `gambar` varchar(255) NOT NULL,
+  `ket_gambar` varchar(255) NOT NULL,
+  `status` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `berita`
+--
+
+INSERT INTO `berita` (`id_berita`, `id_admin`, `judul_artikel`, `jenis_spesialis`, `isiberita`, `tanggal`, `tag`, `gambar`, `ket_gambar`, `status`) VALUES
+(21, 1, 'jhghg', 'PENYAKIT DALAM', '<p>hghgh</p>\r\n', '2023-03-01 03:29:32', 'shgh', '230301035034ttd subarman.png', '', 'T'),
+(25, 1, 'mantap gak tuh', 'BEDAH', '<p><strong>Lorem Ipsum</strong>&nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>\r\n\r\n<h2>Why do we use it?</h2>\r\n\r\n<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using &#39;Content here, content here&#39;, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for &#39;lorem ipsum&#39; will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>\r\n', '2023-03-01 06:48:45', 'sipp', '230301074845ttd selpi.png', 'mantap gaess', 'N'),
+(26, 1, 'jghgdshghs', 'PENYAKIT DALAM', '<p>dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;dsjgdsyghsd dsh dshhsd ghsdghs dsdgh sdghds gh dshsdgdsh gdsh dgsdgs hds ghdgs&nbsp;</p>\r\n', '2023-03-01 07:02:15', 'ok', '2303010802151 (5).jpg', 'gambar mantap', 'Y');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gambar`
+--
+
+CREATE TABLE `gambar` (
+  `id_gambar` int(11) NOT NULL,
+  `nama_gambar` varchar(100) NOT NULL,
+  `gambar` varchar(255) NOT NULL,
+  `ket_gambar` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `gambar`
+--
+
+INSERT INTO `gambar` (`id_gambar`, `nama_gambar`, `gambar`, `ket_gambar`) VALUES
+(1, 'hjg', '201005185931a.png', 'jh'),
+(2, 'ghs', '200922092329eh23.png', 'khjha');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `komentar`
+--
+
+CREATE TABLE `komentar` (
+  `id_komentar` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `komentar` text NOT NULL,
+  `lampiran` varchar(255) NOT NULL,
+  `hp` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `komentar`
+--
+
+INSERT INTO `komentar` (`id_komentar`, `nama`, `komentar`, `lampiran`, `hp`) VALUES
+(1, 'uyhj', 'jhjhj', '200930032704', '67676'),
+(2, 'ghghg', 'nghgsa', '200930033321', '67676876'),
+(3, 'hjajshj', 'hghh', '200930033905', '6777'),
+(4, 'hjhjh', 'jhjjh', '200930033949', '78768768'),
+(5, 'hjhjh', 'jhjjh', '200930034037', '78768768'),
+(6, 'hjhgjh', 'hjh', '2009300340491.PNG', 'y6uhj'),
+(7, 'hjhgjh', 'hjh', '2009300446201.PNG', 'y6uhj'),
+(8, 'hjhgjh', 'hjh', '2009300446351.PNG', 'y6uhj'),
+(9, 'hjhgjh', 'hjh', '2009300446541.PNG', 'y6uhj'),
+(10, 'hjhgjh', 'hjh', '2009300447311.PNG', 'y6uhj');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kontak`
+--
+
+CREATE TABLE `kontak` (
+  `id_kontak` int(11) NOT NULL,
+  `hp` varchar(15) NOT NULL,
+  `alamat` text NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `web` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `kontak`
+--
+
+INSERT INTO `kontak` (`id_kontak`, `hp`, `alamat`, `email`, `web`) VALUES
+(1, '0852257412712', 'disini  disana', 'htaufiq0072@gmail.com', 'www.ok2.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `id_admin` int(15) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `nama` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sejarah`
+--
+
+CREATE TABLE `sejarah` (
+  `id_sejarah` int(11) NOT NULL,
+  `sejarah` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `sejarah`
+--
+
+INSERT INTO `sejarah` (`id_sejarah`, `sejarah`) VALUES
+(1, '<p>&nbsp;</p>\r\n\r\n<p><img alt=\"\" src=\"upload/1497490819.png\" style=\"height:346px; width:397px\" /></p>\r\n\r\n<p>Sejarah Kelurahan Lompoe</p>\r\n\r\n<p>jsdhjdsh</p>\r\n\r\n<p>djhdsj</p>\r\n');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `struktur`
+--
+
+CREATE TABLE `struktur` (
+  `id_struktur` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `jabatan` varchar(100) NOT NULL,
+  `nip` varchar(100) NOT NULL,
+  `id_jab` int(11) NOT NULL,
+  `foto` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `struktur2`
+--
+
+CREATE TABLE `struktur2` (
+  `id` int(11) NOT NULL,
+  `struktur` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `struktur2`
+--
+
+INSERT INTO `struktur2` (`id`, `struktur`) VALUES
+(1, '201005213928STRUKTUR ORGANISASI.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `video`
+--
+
+CREATE TABLE `video` (
+  `id_video` int(11) NOT NULL,
+  `nama_video` int(11) NOT NULL,
+  `video` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `visi`
+--
+
+CREATE TABLE `visi` (
+  `id_visi` int(11) NOT NULL,
+  `visi` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `visi`
+--
+
+INSERT INTO `visi` (`id_visi`, `visi`) VALUES
+(1, '<p><strong>VISI:</strong></p>\r\n\r\n<p>TERWUJUDNYA PELAYANAN PRIMA KEPADA MASYARAKAT DALAM RANGKA MENDUKUNG KOTA BANDAR MADANI DENGAN MASYARAKAT YANG MANDIRI DAN RELIGIUS</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p><strong>MISI:</strong></p>\r\n\r\n<ol>\r\n	<li>MENINGKATKAN SUMBER DAYA MANUSIA, APARAT KELURAHAN YANG MANDIRI, CERDAS, SEHAT DAN BERDAYA SAING</li>\r\n	<li>MEWUJUDKAN MASYARAKAT RELIGIUS, RUKUN, AMAN DAN MANUSIAWI</li>\r\n	<li>MENCIPTAKAN KEPEDULIAN MASYARAKAT DENGAN BERKOMITMEN HEMAT DAN EFESIEN</li>\r\n	<li>MENINGKATKAN PENATAAN RUANG YANG BERWAWASAN LINGKUNGAN</li>\r\n</ol>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>&nbsp;</p>\r\n');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `about`
+--
+ALTER TABLE `about`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indexes for table `berita`
+--
+ALTER TABLE `berita`
+  ADD PRIMARY KEY (`id_berita`);
+
+--
+-- Indexes for table `gambar`
+--
+ALTER TABLE `gambar`
+  ADD PRIMARY KEY (`id_gambar`);
+
+--
+-- Indexes for table `komentar`
+--
+ALTER TABLE `komentar`
+  ADD PRIMARY KEY (`id_komentar`);
+
+--
+-- Indexes for table `kontak`
+--
+ALTER TABLE `kontak`
+  ADD PRIMARY KEY (`id_kontak`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indexes for table `sejarah`
+--
+ALTER TABLE `sejarah`
+  ADD PRIMARY KEY (`id_sejarah`);
+
+--
+-- Indexes for table `struktur2`
+--
+ALTER TABLE `struktur2`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `video`
+--
+ALTER TABLE `video`
+  ADD PRIMARY KEY (`id_video`);
+
+--
+-- Indexes for table `visi`
+--
+ALTER TABLE `visi`
+  ADD PRIMARY KEY (`id_visi`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `about`
+--
+ALTER TABLE `about`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `berita`
+--
+ALTER TABLE `berita`
+  MODIFY `id_berita` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `gambar`
+--
+ALTER TABLE `gambar`
+  MODIFY `id_gambar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `komentar`
+--
+ALTER TABLE `komentar`
+  MODIFY `id_komentar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `kontak`
+--
+ALTER TABLE `kontak`
+  MODIFY `id_kontak` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `id_admin` int(15) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sejarah`
+--
+ALTER TABLE `sejarah`
+  MODIFY `id_sejarah` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `struktur2`
+--
+ALTER TABLE `struktur2`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `video`
+--
+ALTER TABLE `video`
+  MODIFY `id_video` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `visi`
+--
+ALTER TABLE `visi`
+  MODIFY `id_visi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
