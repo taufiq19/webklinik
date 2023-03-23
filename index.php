ce9ddf35a0c4828e6ef2b762924ab7c5a3c2c660
Teha Project <htaufiq007@Gmail.com>
2023-03-24 06:15:41 +0800

    header

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
-<?php
-include 'header.php'
-?>
-
 <!-- <div class="content-wrapper"> -->
 
 <?php
 if (isset($_GET['halaman'])) {
   if ($_GET['halaman'] == "isiartikel") {
+    include 'header2.php';
     include 'isi-artikel.php';
   } elseif ($_GET['halaman'] == "fasilitas") {
+    include 'header2.php';
     include 'UI/fasilitas.php';
   } elseif ($_GET['halaman'] == "tambahartikel") {
+    include 'header2.php';
     include 'Proses/tambahartikel.php';
   }
 } else {
+  include 'header.php';
   include 'home.php';
 }
 ?>
